59dd5c97dfc0d9bb5482c1b78a8058d523725d76
autom8ter <coleman.word@autom8ter.com>
2020-04-16 17:30:27 -0600

    server: implement endpoints: /status?imei and /readings?imei

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,13 +1,14 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/pprof"
+	"strconv"
+	"time"
 )
 
-//setupRoutes adds http handlers to the servers mux
 func (s *Server) setupRoutes() {
-	//pprof endpoints for debugging purposes
 	s.mux.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
 	s.mux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
 	s.mux.Handle("/debug/pps.muxof/profile", http.HandlerFunc(pprof.Profile))
@@ -18,16 +19,51 @@ func (s *Server) setupRoutes() {
 	s.mux.HandleFunc("/stats", s.handleStats())
 }
 
-//handleStatus parses the imei query parameter
 func (s *Server) handleStatus() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//finish implementation
+		id := r.URL.Query().Get("imei")
+		if id == "" {
+			http.Error(w, "missing imei", http.StatusBadRequest)
+			return
+		}
+		uid, err := strconv.ParseUint(id, 0, 64)
+		if err != nil {
+			http.Error(w, "invalid uid", http.StatusBadRequest)
+			return
+		}
+		if reading, ok := s.GetReading(uid); ok {
+			//if a reading has been stored in the past 5 minutes, return 200
+			if time.Since(reading.Timestamp) < 5*time.Minute {
+				w.WriteHeader(http.StatusOK)
+			}
+		} else {
+			http.Error(w, "reading not found", http.StatusNotFound)
+			return
+		}
 	}
 }
 
 func (s *Server) handleReading() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//finish implementation
+		id := r.URL.Query().Get("imei")
+		if id == "" {
+			http.Error(w, "missing imei", http.StatusBadRequest)
+			return
+		}
+		uid, err := strconv.ParseUint(id, 0, 64)
+		if err != nil {
+			http.Error(w, "invalid uid", http.StatusBadRequest)
+			return
+		}
+		if reading, ok := s.GetReading(uid); ok {
+			if err := json.NewEncoder(w).Encode(reading); err != nil {
+				http.Error(w, "failed to encode reading", http.StatusInternalServerError)
+				return
+			}
+		} else {
+			http.Error(w, "reading not found", http.StatusNotFound)
+			return
+		}
 	}
 }
 
